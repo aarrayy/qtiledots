620faa2174e8f56a15225b5bb8399f902f355101
aarrayy <aarrayyzuelo@protonmail.com>
2024-06-10 16:44:37 -0300

    Auto resolution

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 &
+sleep 3
+xrandr --output HDMI-A-0 --set TearFree on &
 feh --bg-fill $HOME/.config/qtile/Wallpaper/Skyscraper.png &
 #picom --daemon --config $HOME/.config/qtile/scripts/picom.conf & 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
